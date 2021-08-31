315642e0d0ae6f14e3f50a3a1b8c7b1d2bd88d7c
Sayem Chowdhury <fr9838@wayne.edu>
2021-08-31 01:25:14 -0400

    Initial commit

---
+++ src/main/java/com/microservice/photoapp/api/users/photoappapiusers/PhotoAppApiUsersApplication.java
@@ -0,0 +1,17 @@
+package com.microservice.photoapp.api.users.photoappapiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan({"com.microservice.photoapp.api.users.ui.controllers"})
+public class PhotoAppApiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
+
+}
